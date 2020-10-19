e7e6c6a9f9581f5f42eb599b5305b5963a08d082
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-10-19 18:34:57 +0800

    comlete scala3. pass locally

--- Task3.scala
+++ Task3.scala
@@ -1,16 +1,48 @@
-import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.mutable.ListBuffer
 
 // please don't change the object name
 object Task3 {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("Task 3")
+    val conf = new SparkConf().setAppName("Task 3").setMaster("local")
     val sc = new SparkContext(conf)
 
-    val textFile = sc.textFile(args(0))
+    // for test locally
+    val inPath = "sample_input/smalldata.txt"
+    val outPath = "my_output/scala3.out"
+    val textFile = sc.textFile(inPath)
+//    val textFile = sc.textFile(args(0))
 
     // modify this code
-    val output = textFile.map(x => x);
-    
-    output.saveAsTextFile(args(1))
+//    val output = textFile.map(line => {
+//      val ratings = line.split(',')
+//      val ratedUserList = new ListBuffer[(Int, Int)]
+//
+//      // idx starts from 0, 0 is movie name
+//      for (i <- 1 until ratings.length) {
+//        if (ratings(i) != "") {
+//          ratedUserList.append((i, 1))
+//        } else {
+//          ratedUserList.append((i, 0))
+//        }
+//      }
+//
+//      ratedUserList.toList
+//    })
+
+    val output = textFile.map(line => line.split(',').drop(1).zipWithIndex.map{case (rating, user) => {
+      var times = 1;
+      if (rating.equals("")) {
+        times = 0;
+      }
+      (user + 1, times)
+    }})
+      .flatMap(x => x)
+      .reduceByKey(_ + _)
+      .map(x => x._1 + "," + x._2)
+
+    output.saveAsTextFile(outPath)
+//    output.saveAsTextFile(args(1))
   }
 }
