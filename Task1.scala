f21b9649ce050c8af48e2511cd714c765e998f10
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-10-19 16:50:47 +0800

    test scala2 locally. pass

--- Task1.scala
+++ Task1.scala
@@ -4,19 +4,9 @@ import scala.collection.mutable.{ListBuffer}
 // please don't change the object name
 object Task1 {
   def main(args: Array[String]) {
-//    val conf = new SparkConf().setAppName("Task 1")
-
-    // for test locally
-    val conf = new SparkConf().setAppName("Task 1").setMaster("local")
-
+    val conf = new SparkConf().setAppName("Task 1")
     val sc = new SparkContext(conf)
-
-    // for test locally
-    val inPath = "sample_input/smalldata.txt"
-    val outPath = "my_output/scala1.out"
-    val textFile = sc.textFile(inPath)
-
-//    val textFile = sc.textFile(args(0))
+    val textFile = sc.textFile(args(0))
 
     // modify this code
     val output = textFile.map(line => {
@@ -44,9 +34,57 @@ object Task1 {
       movie + "," + maxUserList.mkString(",")
     })
     
-//    output.saveAsTextFile(args(1))
-
-    // for test locally
-    output.saveAsTextFile(outPath)
+    output.saveAsTextFile(args(1))
   }
 }
+
+
+/* for local test */
+//object Task1 {
+//  def main(args: Array[String]) {
+//    //    val conf = new SparkConf().setAppName("Task 1")
+//
+//    // for test locally
+//    val conf = new SparkConf().setAppName("Task 1").setMaster("local")
+//
+//    val sc = new SparkContext(conf)
+//
+//    // for test locally
+//    val inPath = "sample_input/smalldata.txt"
+//    val outPath = "my_output/scala1.out"
+//    val textFile = sc.textFile(inPath)
+//
+//    //    val textFile = sc.textFile(args(0))
+//
+//    // modify this code
+//    val output = textFile.map(line => {
+//      val ratings = line.split(',')
+//      val movie = ratings(0)
+//
+//      var max = 0
+//      var maxUserList = new ListBuffer[Int]
+//
+//      // idx starts from 0, 0 is movie name
+//      for (i <- 1 until ratings.length) {
+//        if (ratings(i) != "") {
+//          val rating = ratings(i).toInt
+//          if (rating == max) {
+//            maxUserList += i
+//          } else if (rating > max) {
+//            max = rating
+//            maxUserList.clear()
+//            maxUserList += i
+//            // maxUserList.append(i)
+//          }
+//        }
+//      }
+//
+//      movie + "," + maxUserList.mkString(",")
+//    })
+//
+//    //    output.saveAsTextFile(args(1))
+//
+//    // for test locally
+//    output.saveAsTextFile(outPath)
+//  }
+//}
--- Task2.scala
+++ Task2.scala
@@ -1,4 +1,4 @@
-import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.{SparkConf, SparkContext}
 
 // please don't change the object name
 object Task2 {
@@ -10,13 +10,39 @@ object Task2 {
 
     // modify this code
     val output = textFile.flatMap(line => line.split(',').drop(1))
-                  .filter(rating => rating != "")
+      .filter(rating => rating != "")
+      .count()
 
-//    output.length.saveAsTextFile(args(1))
-
-     val accum = sc.longAccumulator("ratings number count accumulator")
-     sc.parallelize(Seq(output)).foreach(_ => accum.add(1))
-     val count = accum.value
-     textFile.flatMap(s => Seq(count)).saveAsTextFile(args(1))
+    sc.parallelize(Seq(output)).saveAsTextFile(args(1))
   }
 }
+
+//object Task2 {
+//  def main(args: Array[String]) {
+//    val conf = new SparkConf().setAppName("Task 2").setMaster("local")
+//    val sc = new SparkContext(conf)
+//
+//    // for test locally
+//    val inPath = "sample_input/smalldata.txt"
+//    val outPath = "my_output/scala2.out"
+//    val textFile = sc.textFile(inPath)
+//
+//    // modify this code
+//    val output = textFile.flatMap(line => line.split(',').drop(1))
+//      .filter(rating => rating != "")
+//      .count()
+//
+//    sc.parallelize(Seq(output)).saveAsTextFile(outPath)
+//
+//
+////    // wrong
+////    output.length.saveAsTextFile(outPath)
+//
+////    // wrong
+////    val accum = sc.longAccumulator("ratings number count accumulator")
+////    sc.parallelize(Seq(output)).foreach(_ => accum.add(1))
+////    val count = accum.value
+////    println(count)
+////    textFile.flatMap(s => Seq(count)).saveAsTextFile(outPath)
+//  }
+//}
