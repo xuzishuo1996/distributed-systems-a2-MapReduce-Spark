0952c346786fd5edc749ca407016be9eee95d378
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-10-19 12:39:19 +0800

    complete task1.scala, not tested yet

--- Task1.scala
+++ Task1.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.{SparkContext, SparkConf}
+import scala.collection.mutable.{ListBuffer}
 
 // please don't change the object name
 object Task1 {
@@ -9,7 +10,30 @@ object Task1 {
     val textFile = sc.textFile(args(0))
 
     // modify this code
-    val output = textFile.map(x => x);
+    val output = textFile.map(x => {
+      var ratings = x.split(",")
+      var movie = ratings(0)
+
+      var max = 0
+      var maxUserList = new ListBuffer[String]
+
+      // idx starts from 0, 0 is movie name
+      for (i <- 1 until ratings.length) {
+        if (ratings(i) != "") {
+          val rating = ratings(i).toInt
+          if (rating == max) {
+            maxUserList += i
+          } else if (rating > max) {
+            max = rating
+            maxUserList.clear()
+            maxUserList += i
+            // maxUserList.append(i)
+          }
+        }
+      }
+
+      movie + "," + maxUserList.mkString(",")
+    })
     
     output.saveAsTextFile(args(1))
   }
