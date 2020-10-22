1d5fdf0ffa10d54fd25751a3f9038e2fdcf2223a
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-10-22 17:49:30 +0800

    fix Task Not Serializable

--- Task4.scala
+++ Task4.scala
@@ -2,32 +2,32 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 // please don't change the object name
 object Task4 {
-  def calcSimilarity(array1: Array[String], array2:  Array[String]): String = {
-    val ratings1 = array1.drop(1).map(rating => {
-      if (rating.nonEmpty) {
-        rating.toInt
-      } else {
-        0
-      }
-    })
-
-    val ratings2 = array2.drop(1).map(rating => {
-      if (rating.nonEmpty) {
-        rating.toInt
-      } else {
-        0
-      }
-    })
-
-    var similarity = 0
-    for (i <- ratings1.indices) {
-      if (ratings1(i) != 0 && ratings1(i) == ratings2(i)) {
-        similarity += 1
-      }
-    }
-
-    array1(0) + ',' + array2(0) + ',' + similarity
-  }
+//  def calcSimilarity(array1: Array[String], array2:  Array[String]): String = {
+//    val ratings1 = array1.drop(1).map(rating => {
+//      if (rating.nonEmpty) {
+//        rating.toInt
+//      } else {
+//        0
+//      }
+//    })
+//
+//    val ratings2 = array2.drop(1).map(rating => {
+//      if (rating.nonEmpty) {
+//        rating.toInt
+//      } else {
+//        0
+//      }
+//    })
+//
+//    var similarity = 0
+//    for (i <- ratings1.indices) {
+//      if (ratings1(i) != 0 && ratings1(i) == ratings2(i)) {
+//        similarity += 1
+//      }
+//    }
+//
+//    array1(0) + ',' + array2(0) + ',' + similarity
+//  }
 
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 4")
@@ -39,7 +39,33 @@ object Task4 {
     val lines = textFile.map(line => line.split(",", -1)).persist()
     val output = lines.cartesian(lines)
         .filter(pair => pair._1(0).compareTo(pair._2(0)) < 0)
-        .map(pair => calcSimilarity(pair._1, pair._2))
+        .map(pair => {
+          
+          val ratings1 = pair._1.drop(1).map(rating => {
+            if (rating.nonEmpty) {
+              rating.toInt
+            } else {
+              0
+            }
+          })
+
+          val ratings2 = pair._2.drop(1).map(rating => {
+            if (rating.nonEmpty) {
+              rating.toInt
+            } else {
+              0
+            }
+          })
+
+          var similarity = 0
+          for (i <- ratings1.indices) {
+            if (ratings1(i) != 0 && ratings1(i) == ratings2(i)) {
+              similarity += 1
+            }
+          }
+
+          pair._1(0) + ',' + pair._2(0) + ',' + similarity
+        })
 
     output.saveAsTextFile(args(1))
   }
